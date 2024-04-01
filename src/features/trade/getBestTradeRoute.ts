129e8f07a3b5040fff48a77fd1be6b3668fba81e
Stafford Williams <stafford.williams@gmail.com>
2024-04-02 01:23:04 +1100

    add pathfinding & route finding

---
+++ src/features/trade/getBestTradeRoute.ts
@@ -0,0 +1,127 @@
+import lodash from 'lodash'
+import { TradeSymbol } from '../../../api'
+import { ShipEntity } from '../ship/ship.entity'
+import { distanceWaypoint, getGraph, getShortestPath, getTravelTime } from '../waypoints/pathfinding'
+import { WaypointEntity } from '../waypoints/waypoint.entity'
+
+type TradeRoute = {
+  tradeSymbol: TradeSymbol
+  fuelCost: number
+  buyLocation: WaypointEntity
+  purchasePricePerUnit: number
+  sellLocation: WaypointEntity
+  sellPricePerUnit: number
+  distance: number
+  volume: number
+  profitPerUnit: number
+  totalProfit: number
+  costVolumeDistance: number
+  quantityAvailable: number
+  quantityToBuy: number
+  fuelNeeded: number
+  rank?: number
+}
+
+export async function getBestTradeRoute(ship: ShipEntity, waypoints: WaypointEntity[], excludeLoss = true): Promise<TradeRoute[]> {
+  const goodLocation: TradeGoodWaypoint[] = waypoints
+    .filter((x) => x.tradeGoods && x.tradeGoods.length)
+    .map((x) => ({ waypoint: x, tradeGoods: x.tradeGoods! }))
+    .flatMap((x) => x.tradeGoods?.map((g): TradeGoodWaypoint => ({ waypoint: x.waypoint, tradeGood: g, tradeSymbol: g.symbol })))
+  const grouped = groupByGood(goodLocation)
+  const { graph } = getGraph(waypoints)
+  //const { focusTradeRoute } = getDebug()
+
+  return (
+    grouped
+      .map((g) =>
+        g.locations
+          .map((depart) =>
+            g.locations
+              .filter((dest) => dest.waypoint !== depart.waypoint)
+              .map((dest) => {
+                const volume = depart.tradeGood.tradeVolume
+                const route = getShortestPath(graph, depart.waypoint.symbol, dest.waypoint.symbol, ship)
+                const distance = route.map((p) => distanceWaypoint(p.from, p.to)).reduce((a, b) => a + b)
+                const maxFuelNeeded = Math.max(...route.map((a) => a.fuelNeeded))
+
+                const quantityToBuy = Math.floor(ship.cargo.capacity / volume)
+                const fuelCost = getFuelPriceForRoute(route, waypoints)
+                const profitPerUnit = dest.tradeGood.sellPrice - depart.tradeGood.purchasePrice - fuelCost / quantityToBuy
+                const flightTime = lodash.sumBy(route, (x) => getTravelTime(x.from, x.to, ship))
+
+                const costVolumeDistance = profitPerUnit / volume / flightTime
+                const totalProfit = quantityToBuy * profitPerUnit
+                const result: TradeRoute = {
+                  buyLocation: depart.waypoint,
+                  purchasePricePerUnit: depart.tradeGood.purchasePrice,
+                  sellLocation: dest.waypoint,
+                  sellPricePerUnit: dest.tradeGood.sellPrice,
+                  distance,
+                  tradeSymbol: g.tradeSymbol,
+                  profitPerUnit,
+                  volume,
+                  fuelCost: fuelCost / quantityToBuy,
+                  costVolumeDistance,
+                  quantityAvailable: depart.tradeGood.tradeVolume,
+                  fuelNeeded: maxFuelNeeded,
+                  quantityToBuy,
+                  totalProfit,
+                }
+                return result
+              })
+              .flat(),
+          )
+          .flat(),
+      )
+      .flat()
+      .filter((a) => !excludeLoss || a.costVolumeDistance > 0)
+      // .filter(
+      //   (a) =>
+      //     !focusTradeRoute ||
+      //     (focusTradeRoute.from === a.buyLocation && focusTradeRoute.to === a.sellLocation && focusTradeRoute.good === a.good),
+      // )
+      .sort((a, b) => b.costVolumeDistance - a.costVolumeDistance)
+      .map((r, ix) => ({
+        ...r,
+        rank: ix + 1,
+      }))
+  )
+}
+
+function getFuelPriceForRoute(route: ReturnType<typeof getShortestPath>, waypoints: WaypointEntity[]) {
+  const fuelPrices = waypoints
+    .filter((w) => w.tradeGoods?.find((g) => g.symbol === TradeSymbol.Fuel))
+    .map((x) => ({ waypointSymbol: x.symbol, fuelPrice: x.tradeGoods!.find((g) => g.symbol === TradeSymbol.Fuel)!.purchasePrice }))
+
+  return lodash.sum(
+    route.map((r) => {
+      fuelPrices.find((x) => x.waypointSymbol === r.from.symbol)?.fuelPrice ?? 100 // TODO: need a better fuel price calculation
+    }),
+  )
+}
+
+function groupByGood(market: TradeGoodWaypoint[] | undefined) {
+  const grouped: GroupByGood = {}
+  market?.reduce(function (res: GroupByGood, value: TradeGoodWaypoint) {
+    if (!res[value.tradeSymbol]) {
+      res[value.tradeSymbol] = {
+        tradeSymbol: value.tradeSymbol,
+        locations: [],
+      }
+      grouped[value.tradeSymbol] = res[value.tradeSymbol]
+    }
+    res[value.tradeSymbol]!.locations.push(value)
+    return res
+  }, {})
+  return Object.values(grouped)
+}
+
+type PartialRecord<K extends string | number | symbol, T> = { [P in K]?: T }
+
+type TradeGoodWaypoint = {
+  tradeSymbol: TradeSymbol
+  waypoint: WaypointEntity
+  tradeGood: Exclude<WaypointEntity['tradeGoods'], undefined>[0]
+}
+
+type GroupByGood = PartialRecord<TradeSymbol, { locations: TradeGoodWaypoint[]; tradeSymbol: TradeSymbol }>
---
+++ src/features/waypoints/pathfinding.ts
@@ -0,0 +1,104 @@
+import createGraph from 'ngraph.graph'
+import { aStar } from 'ngraph.path'
+import { ShipNavFlightMode } from '../../../api'
+import { ShipEntity } from '../ship/ship.entity'
+import { WaypointEntity } from './waypoint.entity'
+
+export const getGraph = (waypoints: WaypointEntity[]) => {
+  const graph = createGraph()
+  waypoints.forEach((waypoint) => {
+    graph.addNode(waypoint.symbol, waypoint)
+  })
+
+  return { graph }
+}
+
+type Route = {
+  from: WaypointEntity
+  to: WaypointEntity
+  fuelNeeded: number
+}
+
+export const getShortestPath = (graph: ReturnType<typeof getGraph>['graph'], from: string, to: string, ship: ShipEntity) => {
+  const pathFinder = aStar<WaypointEntity, WaypointEntity>(graph, {
+    distance(fromNode, toNode) {
+      const distance = distanceWaypoint(fromNode.data, toNode.data)
+      const fuelCost = getFuelNeeded(fromNode.data, toNode.data, ship)
+      if (fuelCost > ship.fuel.capacity) return Infinity
+      return distance
+    },
+    heuristic(fromNode, toNode) {
+      const dx = fromNode.data.x - toNode.data.x
+      const dy = fromNode.data.y - toNode.data.y
+      return Math.sqrt(dx * dx + dy * dy)
+    },
+  })
+  const path = pathFinder.find(from, to).reverse()
+
+  const route: Route[] = []
+
+  path.forEach((p, ix) => {
+    if (ix) {
+      const from = path[ix - 1].data
+      const to = p.data
+      const fuelNeeded = getFuelNeeded(from, to, ship)
+      //const fuelAvailable = fuelCache[from.symbol]?.available ?? 0
+      route.push({
+        from,
+        to,
+        fuelNeeded,
+      })
+    }
+  })
+  return route
+}
+
+function getFuelNeeded(from: WaypointEntity, to: WaypointEntity, ship: ShipEntity) {
+  if (ship.fuel.capacity === 0) return 0 // satelites don't appear to need fuel
+  const distance = distanceWaypoint(from, to)
+  switch (ship.nav.flightMode) {
+    case ShipNavFlightMode.Cruise:
+    case ShipNavFlightMode.Stealth:
+      return Math.max(1, Math.round(distance))
+    case ShipNavFlightMode.Drift:
+      return 1
+    case ShipNavFlightMode.Burn:
+      return Math.max(1, 2 * Math.round(distance))
+    default:
+      throw new Error(`Unknown flight mode: ${ship.nav.flightMode}`)
+  }
+}
+
+function getNavigationMultipler(ship: ShipEntity) {
+  switch (ship.nav.flightMode) {
+    case ShipNavFlightMode.Cruise:
+      return 25
+    case ShipNavFlightMode.Stealth:
+      return 30
+    case ShipNavFlightMode.Drift:
+      return 250
+    case ShipNavFlightMode.Burn:
+      return 12.5
+    default:
+      throw new Error(`Unknown flight mode: ${ship.nav.flightMode}`)
+  }
+}
+
+export function getTravelTime(from: WaypointEntity, to: WaypointEntity, ship: ShipEntity) {
+  const distance = distanceWaypoint(from, to)
+  return Math.round(Math.round(Math.max(1, distance)) * (getNavigationMultipler(ship) / ship.engine.speed) + 15)
+}
+
+export function distanceWaypoint(from: WaypointEntity, to: WaypointEntity) {
+  return distancePoint(from, to)
+}
+
+type Point = { x: number; y: number }
+
+export const distancePoint = (p1: Point, p2: Point) => {
+  return distance(p1.x, p1.y, p2.x, p2.y)
+}
+
+const distance = (x1: number, y1: number, x2: number, y2: number) => {
+  return Math.sqrt((x2 -= x1) * x2 + (y2 -= y1) * y2)
+}
