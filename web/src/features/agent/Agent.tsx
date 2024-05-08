68a23eef3621c0f8958c022f5667ad2ddc9f15ad
Stafford Williams <stafford.williams@gmail.com>
2024-05-08 23:38:01 +1000

    move markets

--- web/src/features/agent/Agent.tsx
+++ web/src/features/agent/Agent.tsx
@@ -33,8 +33,8 @@ export const Agent = () => {
             )}
           />
           <Waypoints />
-          <Markets />
           <JumpGate />
+          <Markets />
         </Stack>
       )}
     />
