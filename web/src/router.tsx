30014b868f702a36d06b019f4c62a98670c6209f
Stafford Williams <stafford.williams@gmail.com>
2024-05-15 01:24:21 +1000

    base

--- web/src/router.tsx
+++ web/src/router.tsx
@@ -9,60 +9,63 @@ import { Markets } from './features/agent/Markets.tsx'
 import { Raw } from './features/agent/Raw.tsx'
 import { System } from './features/agent/System.tsx'
 
-export const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: 'raw',
-        element: (
-          <Suspense>
-            <Raw atom={agentAtom} />
-          </Suspense>
-        ),
-      },
-      {
-        path: ':systemSymbol',
-        element: <System />,
-        children: [
-          {
-            path: 'markets',
-            element: (
-              <Suspense>
-                <Markets />
-              </Suspense>
-            ),
-            children: [
-              {
-                path: ':marketSymbol',
-                element: (
-                  <Suspense>
-                    <Market />
-                  </Suspense>
-                ),
-              },
-            ],
-          },
-          {
-            path: 'jump-gate',
-            element: (
-              <Suspense>
-                <JumpGate />
-              </Suspense>
-            ),
-          },
-          {
-            path: 'raw',
-            element: (
-              <Suspense>
-                <Raw atom={systemAtom} />
-              </Suspense>
-            ),
-          },
-        ],
-      },
-    ],
-  },
-])
+export const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <App />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          path: 'raw',
+          element: (
+            <Suspense>
+              <Raw atom={agentAtom} />
+            </Suspense>
+          ),
+        },
+        {
+          path: ':systemSymbol',
+          element: <System />,
+          children: [
+            {
+              path: 'markets',
+              element: (
+                <Suspense>
+                  <Markets />
+                </Suspense>
+              ),
+              children: [
+                {
+                  path: ':marketSymbol',
+                  element: (
+                    <Suspense>
+                      <Market />
+                    </Suspense>
+                  ),
+                },
+              ],
+            },
+            {
+              path: 'jump-gate',
+              element: (
+                <Suspense>
+                  <JumpGate />
+                </Suspense>
+              ),
+            },
+            {
+              path: 'raw',
+              element: (
+                <Suspense>
+                  <Raw atom={systemAtom} />
+                </Suspense>
+              ),
+            },
+          ],
+        },
+      ],
+    },
+  ],
+  { basename: '/spacetraders' },
+)
